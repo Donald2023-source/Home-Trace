8fde9ad6f8c5cdd84d89e8ae01e5202eed3196c1
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-25 13:21:39 +0100

    changes

--- app/(Pages)/auth/page.tsx
+++ app/(Pages)/auth/page.tsx
@@ -1,28 +1,51 @@
+"use client";
 import React from "react";
 import img1 from "@/public/Frame 18773.png";
 import Image from "next/image";
 import logo from "@/public/logo.png";
-
+import user from "@/public/Vector (5).png";
+import { useState } from "react";
 const page = () => {
+  const [selectedRole, setSelectedRole] = useState("");
+
+  const handleRole = (role: string) => {
+    setSelectedRole(role.toLowerCase());
+    console.log(role);
+  };
+
   return (
     <div className="flex items-center">
       <div className="w-[45%] border h-screen">
         <Image className="h-full" src={img1} alt="image" />
       </div>
       <div className="p-10 flex-1">
-        <div className="w-[90%] border">
+        <div className="w-[85%]">
           <Image src={logo} alt="logo" className="h-20 w-20" />
           <div>
-            <h1 className="py-5 text-3xl">Welcome To Home Trace</h1>
-            <p className="text-gray-500">
+            <h1 className="py-6 text-3xl">Welcome To Home Trace</h1>
+            <p className="text-gray-500 pb-4">
               Select an Option which Best Describes You
             </p>
 
-            <div>
-                <div>
-                    <p>Agent</p>
-                </div>
+            <div className="w-full flex flex-col gap-5">
+              <div
+                onClick={() => handleRole("agent")}
+                className="flex p-3 bg-primary/20 hover:bg-primary/60 cursor-pointer rounded-lg items-center justify-between"
+              >
+                <p className=" font-semibold">Agent</p>
+                <Image className="h-5 w-5" src={user} alt="User Image" />
+              </div>
+              <div
+                onClick={() => handleRole("user")}
+                className="flex p-3 bg-primary/20 hover:bg-primary/60 cursor-pointer rounded-lg items-center justify-between"
+              >
+                <p className=" font-semibold">User</p>
+                <Image className="h-5 w-5" src={user} alt="User Image" />
+              </div>
             </div>
+            <button className="py-3 px-10 w-full my-8 cursor-pointer bg-primary text-white hover:scale-95 transition rounded-lg">
+              Next
+            </button>
           </div>
         </div>
       </div>
