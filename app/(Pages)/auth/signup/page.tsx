9d64217099c0ff5ffce9e0fe853e4245aefd6a3d
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-26 17:18:09 +0100

    user signup

--- app/(Pages)/auth/signup/page.tsx
+++ app/(Pages)/auth/signup/page.tsx
@@ -31,7 +31,7 @@ const page = () => {
       <div className="md:p-10 p-5 w-[90%] shadow md:shadow-none flex-1 relative md:-top-0  -top-20 bg-white/80 backdrop-blur-md rounded-xl">
         <div className="md:w-[85%] md:block flex flex-col items-center w-[95%]">
           <div className="flex flex-col items-center justify-center">
-            <h1 className="py-6 font-bold text-primary md:text-3xl md:text-left text-center">
+            <h1 className="py-6 font-bold text-primary text-xl md:text-3xl md:text-left text-center">
               Create Account
             </h1>
             <p className="text-gray-500 md:text-base text-sm md:text-left text-center pb-4">
@@ -40,7 +40,7 @@ const page = () => {
           </div>
 
           <form
-            className="w-[80%] mx-auto flex flex-col gap-6 items-center justify-center"
+            className="md:w-[80%] w-full px-7 md:text-base tex-xs mx-auto flex flex-col gap-6 items-center justify-center"
             action=""
           >
             <fieldset className="border w-full p-3 rounded-lg">
