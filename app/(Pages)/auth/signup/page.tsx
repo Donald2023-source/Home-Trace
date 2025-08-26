702ddb77bb139d3437069afbcac8f5942ca16f45
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-26 16:44:40 +0100

    user auth

--- app/(Pages)/auth/signup/page.tsx
+++ app/(Pages)/auth/signup/page.tsx
@@ -12,8 +12,7 @@ const page = () => {
       </div>
       <div className="md:p-10 p-5 w-[90%] shadow md:shadow-none flex-1 relative md:-top-0  -top-20 bg-white/80 backdrop-blur-md rounded-xl">
         <div className="md:w-[85%] md:block flex flex-col items-center w-[95%]">
-          <Image src={logo} alt="logo" className="h-20 w-20" />
-          <div>
+          <div className="flex flex-col items-center justify-center">
             <h1 className="py-6 font-bold text-primary md:text-3xl md:text-left text-center">
               Create Account
             </h1>
@@ -22,9 +21,27 @@ const page = () => {
             </p>
           </div>
 
-          <form action="">
-            <fieldset>
-              <input type="text" placeholder="" />
+          <form className="w-[80%] mx-auto flex flex-col gap-5 items-center justify-center" action="">
+            <fieldset className="border w-full p-3 rounded-lg">
+              <input
+                className="w-full h-full outline-none"
+                type="text"
+                placeholder="Full Name"
+              />
+            </fieldset>
+            <fieldset className="border w-full p-3 rounded-lg">
+              <input
+                className="w-full h-full outline-none"
+                type="text"
+                placeholder="email@gmail.com"
+              />
+            </fieldset>
+             <fieldset className="border w-full p-3 rounded-lg">
+              <input
+                className="w-full h-full outline-none"
+                type="text"
+                placeholder="Password"
+              />
             </fieldset>
           </form>
         </div>
