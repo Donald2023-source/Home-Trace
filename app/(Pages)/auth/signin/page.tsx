d5e00c126cda38013475d151b0e7ee8e2a7f07c3
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-28 07:33:45 +0100

    sign in

--- app/(Pages)/auth/signin/page.tsx
+++ app/(Pages)/auth/signin/page.tsx
@@ -1,16 +1,112 @@
-import React from "react";
+"use client";
+import React, { FormEvent, useState } from "react";
+import img1 from "@/public/Frame 18773.png";
+import Image from "next/image";
+import logo from "@/public/logo.png";
+import { ArrowLeft, Eye, EyeOff } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+import { toast } from "sonner";
 
 const page = () => {
+  const [passwordToggle, setPasswordToggle] = useState(false)
+  const [formData, setFormData] = useState({
+    FullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+
+  const handleSubmit = () => {
+    console.log("Go to the next page");
+  };
+
   return (
-    <div className="flex items-center">
-      <div>
-        {/* Side Image */}
-        Side Image
+    <div className="flex md:flex-row flex-col items-center">
+      <div className="md:w-[45%] h-82 md:h-screen">
+        <Image className="h-full object-cover" src={img1} alt="image" />
       </div>
-      <div>
-        {/* form */}
-        Form
-      </div>
+     
+        <div className="md:p-10 p-5 w-[90%] shadow md:shadow-none flex-1 relative md:-top-0  -top-20 bg-white/80 backdrop-blur-md rounded-xl">
+          <div className="md:w-[85%] md:block flex flex-col items-center w-[95%]">
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="py-6 font-bold text-primary text-xl md:text-3xl md:text-left text-center">
+                Sign in
+              </h1>
+            </div>
+
+            <form
+              className="md:w-[80%] w-full px-1 md:text-base text-sm mx-auto flex flex-col gap-6 items-center justify-center"
+              action="POST"
+              onSubmit={() => handleSubmit()}
+            >
+              <fieldset className="border w-full p-3 rounded-lg">
+                <input
+                  className="w-full h-full outline-none"
+                  type="text"
+                  placeholder="Full Name"
+                />
+              </fieldset>
+                <fieldset className="border w-full p-3 rounded-lg">
+                <input
+                  className="w-full h-full outline-none"
+                  type="email"
+                  placeholder="email@gmail.com"
+                />
+                </fieldset>
+               
+                <fieldset className="border flex items-center w-full p-3 rounded-lg">
+                <input
+                  className="w-full h-full outline-none"
+                  type={passwordToggle ? "text" : "password"}
+                  placeholder="Password"
+                />
+                {passwordToggle ? (
+                  <Eye
+                  onClick={() => setPasswordToggle(!passwordToggle)}
+                  className="text-gray-400 cursor-pointer"
+                  />
+                ) : (
+                  <EyeOff
+                  onClick={() => setPasswordToggle(!passwordToggle)}
+                  className="text-gray-400 cursor-pointer"
+                  />
+                )}
+                </fieldset>
+
+                <fieldset className="border flex items-center w-full p-3 rounded-lg">
+                <input
+                  className="w-full h-full outline-none"
+                  type={passwordToggle ? "text" : "password"}
+                  placeholder="Confirm Password"
+                />
+                {passwordToggle ? (
+                  <Eye
+                  onClick={() => setPasswordToggle(!passwordToggle)}
+                  className="text-gray-400 cursor-pointer"
+                  />
+                ) : (
+                  <EyeOff
+                  onClick={() => setPasswordToggle(!passwordToggle)}
+                  className="text-gray-400 cursor-pointer"
+                  />
+                )}
+                </fieldset>
+
+              <Button className="w-full cursor-pointer py-3 rounded-lg">Sign up</Button>
+
+              <p className="text-gray-400">
+                Already have an account?{" "}
+                <Link className="text-primary" href={"/auth/signin"}>
+                  Sign in
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+     
     </div>
   );
 };
