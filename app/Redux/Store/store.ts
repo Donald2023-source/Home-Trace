9e8a3228a471cf68018655169bbfdd9b4a5dd48d
Donald-Source2023 <donalddyusuf@gmail.com>
2025-09-08 16:18:24 +0100

    feat: react-persist setup

--- app/Redux/Store/store.ts
+++ app/Redux/Store/store.ts
@@ -1,11 +1,61 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../authSlice/authSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import authSlice from "@/app/Redux/authSlice/authSlice"
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  WebStorage,
+} from "redux-persist";
+import createWebStorage from "redux-persist/es/storage/createWebStorage";
+
+
+export function createPersistStore(): WebStorage {
+  const isServer = typeof window === "undefined";
+
+  // will returns our dummy server
+  if (isServer) {
+    return {
+      getItem() {
+        return Promise.resolve(null);
+      },
+      setItem() {
+        return Promise.resolve();
+      },
+      removeItem() {
+        return Promise.resolve();
+      },
+    };
+  }
+  return createWebStorage("local");
+}
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createPersistStore();
+
+const persistConfig = {
+  key: "home-trace",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authSlice);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    frentals: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
