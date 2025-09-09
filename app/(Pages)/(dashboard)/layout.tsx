8b77392e6d901965ded5261823097b2db49ede85
Donald-Source2023 <donalddyusuf@gmail.com>
2025-09-09 12:58:11 +0100

    feat: dashboard layout

--- app/(Pages)/(dashboard)/layout.tsx
+++ app/(Pages)/(dashboard)/layout.tsx
@@ -1,20 +1,25 @@
+"use client";
 import Dashboardheader from "@/app/Components/Dashboardheader";
 import Sidebar from "@/app/Components/Sidebar";
+import { usePathname } from "next/navigation";
 
 export default function Dashboardlayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const path = usePathname();
   return (
     <div className="flex ">
-      <section className="w-[20%] h-82 border">
-        <Sidebar />
-      </section>
+      {path === "/home" && (
+        <section className="w-[20%] h-82 border">
+          <Sidebar />
+        </section>
+      )}
       <div className="flex-1">
         <section className="border p-3">
           <Dashboardheader />
-        </section>  
+        </section>
         <div>{children}</div>
       </div>
     </div>
