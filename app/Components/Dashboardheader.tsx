72548c8074361febb4a33410863a549b1fd02316
Donald-Source2023 <donalddyusuf@gmail.com>
2025-09-12 16:10:11 +0100

    fix: user auth state

--- app/Components/Dashboardheader.tsx
+++ app/Components/Dashboardheader.tsx
@@ -1,8 +1,13 @@
+"use client";
 import { ChevronDown, Search } from "lucide-react";
 import React from "react";
 import User from "@/public/Frame 44.png";
 import Image from "next/image";
+import { useSelector } from "react-redux";
+import { RootState } from "../Redux/Store/store";
 const Dashboardheader = () => {
+  const user = useSelector((state: RootState) => state?.auth);
+  console.log(user);
   return (
     <div className="w-full flex items-center justify-between">
       <div className="w-[70%]">
@@ -19,7 +24,7 @@ const Dashboardheader = () => {
       </div>
       <div className="flex items-center gap-3 cursor-pointer">
         <Image className="" src={User} alt="profile" />
-        <h4 className="font-medium tracking-tight">Richard Ade</h4>
+        <h4 className="font-medium tracking-tight">{user?.user?.fullName}</h4>
         <ChevronDown />
       </div>
     </div>
